92bba5e8a3ecbc1a8a5d9a5b42d16bfba9f79936
nik0n <alekss2001@mail.ru>
2020-12-27 03:11:34 +0400

    Make finally version

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,44 @@ function displayOnNewWindow(text) {
     newWin.document.write(text);
 }
 
+function counter(iterable){
+    const symbols ={}
+    for (let symbol of iterable) {
+        if (symbol in symbols){
+            symbols[symbol]++
+        }else{
+            symbols[symbol] = 1
+        }
+    }
+    return symbols
+}
+
+function zfill2(object) {
+    return object.toString().padStart(2, '0')
+}
+
+function format_time(date) {
+    return `${zfill2(date.getHours())}:${zfill2(date.getMinutes())}:${zfill2(date.getSeconds())}`
+}
+
 btn1.onclick = function () {
     words = inputForm.value.split(' ').filter(word => word.length <= 5);
     displayOnNewWindow(words);
 }
 
 btn2.onclick = function () {
-
+    let MaxLetterCount = -Infinity
+    let MaxLetter
+    for (let [letter, letterCount] of Object.entries(counter(inputForm.value))){
+        if (!letter.match(/^[a-zа-я]+$/i)){
+            continue
+        }
+        if (letterCount > MaxLetterCount){
+            MaxLetterCount = letterCount
+            MaxLetter = letter
+        }
+    }
+    displayOnNewWindow(inputForm.value.split(MaxLetter).join(''))
 }
 
 btn3.onclick = function () {
@@ -28,14 +59,6 @@ btn5.onclick = function () {
 
 btn6.onclick = () => {
     result.value = `${eval(calculator.value)}`
-}
-
-function zfill2(object) {
-    return object.toString().padStart(2, '0')
-}
-
-function format_time(date) {
-    return `${zfill2(date.getHours())}:${zfill2(date.getMinutes())}:${zfill2(date.getSeconds())}`
 }
 
 setInterval(() => {
